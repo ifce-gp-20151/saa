582445b4b4dabeafb0f07b7f843716ab066b5b34
Átila Camurça <camurca.home@gmail.com>
2015-04-15 11:32:43 -0300

    Fixed #22.
    Ao se logar usuário é redirecionado para sua tela principal.
    
    Na tela de Psicologia o usuário tem acesso ao aluno através do menu.

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -4,6 +4,7 @@ namespace Application\Controller;
 
 use Core\Controller\ActionController;
 use Zend\View\Model\ViewModel;
+use Application\Service\Auth;
 
 class IndexController extends ActionController {
 
@@ -12,7 +13,14 @@ class IndexController extends ActionController {
      *    /
      */
     public function indexAction() {
-
+        $session = $this->getServiceLocator()->get('Session');
+        $user = $session->offsetGet('user');
+        switch ($user->role_id) {
+            case Auth::ROLE_ID_ADMIN:
+                return $this->redirect()->toRoute('admin');
+            case Auth::ROLE_ID_PSICOLOGIA:
+                return $this->redirect()->toRoute('psicologia');
+        }
     }
 
     /**
